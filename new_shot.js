8126ec8df5ccd79b12adcc96984ab8541a3a378f
Abolfazl Mahmoodi <msd.soft@gmail.com>
2016-04-02 08:41:39 +0000

    3

--- new_shot.js
+++ new_shot.js
@@ -1,17 +1,11 @@
 var webPage = require('webpage');
 var page = webPage.create();
 console.log('1');
-page.open('http://abmahmoodi.koding.io:3000/generate_chart/create_chart', function(status) {
-  console.log('2');
-  if (status === "success") {
-    page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function() {
-      console.log('3');
-      // page.includeJs('https://www.gstatic.com/charts/loader.js', function() {
-        console.log('4');
-        page.render('github.png');
-        console.log('OK');
+page.setContent('<html><head></head><body><h1>HELLO</h1></body></html>', 'http://google.com');
+
+page.open('onLoadFinished', function() {
+      return page.render('mychart.png', function() {
         phantom.exit();
-      // });
+        return process.exit(0);
+      });
     });
-  } else { console.log('ERROR') }
-});
