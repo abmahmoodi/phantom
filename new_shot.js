038d45815b6bd0708e92034d10366cccfb11c0a7
Abolfazl Mahmoodi <msd.soft@gmail.com>
2016-04-02 10:11:09 +0000

    4

--- new_shot.js
+++ new_shot.js
@@ -1,11 +1,13 @@
 var webPage = require('webpage');
 var page = webPage.create();
 console.log('1');
-page.setContent('<html><head></head><body><h1>HELLO</h1></body></html>', 'http://google.com');
 
-page.open('onLoadFinished', function() {
-      return page.render('mychart.png', function() {
-        phantom.exit();
-        return process.exit(0);
-      });
-    });
+//page.setContent('<html><head></head><body><h1>HELLO</h1> <div id="chart_div"></div> </body></html>', 'http://google.com');
+
+page.viewportSize = { width: 1920, height: 1080 };
+page.open('http://google.com', function() {
+  console.log('2');
+  page.render('mychart1.png');
+  console.log('3');
+  phantom.exit();
+});
