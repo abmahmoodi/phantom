070055ba73e993f8719743b72bc978d39c1cbef7
Abolfazl Mahmoodi <msd.soft@gmail.com>
2016-04-02 10:44:54 +0000

    7

--- new_shot.js
+++ new_shot.js
@@ -5,8 +5,8 @@ console.log('1');
 //page.setContent('<html><head></head><body><h1>HELLO</h1> <div id="chart_div"></div> </body></html>', 'http://google.com');
 
 page.viewportSize = { width: 400, height: 400 };
-page.content = '<html><head></head><body><h1>HELLO</h1> <div id="chart_div"></div> </body></html>';
+page.content = '<html><head></head><body><h1 id="test">HELLO</h1> <div id="chart_div"></div> <script>var c=document.getElementById("chart_div"); c.innerText="TEST";</script></body></html>';
   console.log('2');
-  page.render('mychart3.png');
+  page.render('mychart4.png');
   console.log('3');
   phantom.exit();
